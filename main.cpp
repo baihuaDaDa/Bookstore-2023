3290debeea1bba7b236cbedc42b624d58fd2efca
baihua <zhangbohan_wx@sjtu.edu.cn>
2023-12-25 00:14:35 +0800

    Upd: do not use the directory database.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ int main() {
 //    freopen("/mnt/c/Users/zwg_w/Desktop/CLion/OJ Homework_big/5-Bookstore/Bookstore-2023/data/basic/testcase7/3.in", "r", stdin);
 //    freopen("/mnt/c/Users/zwg_w/Desktop/CLion/OJ Homework_big/5-Bookstore/Bookstore-2023/data/basic/testcase7/3.out", "w", stdout);
     std::cout << std::fixed << std::setprecision(2);
-    System system("database/UserIndex.bin", "database/UserElement.bin",
-                  "database/ISBNIndex.bin", "database/ISBNElement.bin",
-                  "database/BookNameIndex.bin", "database/BookNameElement.bin",
-                  "database/AuthorNameIndex.bin", "database/AuthorNameElement.bin",
-                  "database/KeywordIndex.bin", "database/KeywordElement.bin",
-                  "database/Finance.bin", "database/FinanceReport.bin",
-                  "database/EmployeeWorkIndex.bin", "database/EmployeeWorkElement.bin",
-                  "database/Log.bin");
+//    System system("database/UserIndex.bin", "database/UserElement.bin",
+//                  "database/ISBNIndex.bin", "database/ISBNElement.bin",
+//                  "database/BookNameIndex.bin", "database/BookNameElement.bin",
+//                  "database/AuthorNameIndex.bin", "database/AuthorNameElement.bin",
+//                  "database/KeywordIndex.bin", "database/KeywordElement.bin",
+//                  "database/Finance.bin", "database/FinanceReport.bin",
+//                  "database/EmployeeWorkIndex.bin", "database/EmployeeWorkElement.bin",
+//                  "database/Log.bin");
+    System system("UserIndex.bin", "UserElement.bin",
+                  "ISBNIndex.bin", "ISBNElement.bin",
+                  "BookNameIndex.bin", "BookNameElement.bin",
+                  "AuthorNameIndex.bin", "AuthorNameElement.bin",
+                  "KeywordIndex.bin", "KeywordElement.bin",
+                  "Finance.bin", "FinanceReport.bin",
+                  "EmployeeWorkIndex.bin", "EmployeeWorkElement.bin",
+                  "Log.bin");
     system.initialise();
 //    InstructionScanner scanner;
 //    scanner.IsDouble("49.8", PRICE);
